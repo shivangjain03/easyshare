37e48722124774d1aed19ff3e02c61964976056e
Shivang Jain <129207095+shivangjain03@users.noreply.github.com>
2024-11-17 08:40:58 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: ['firebasestorage.googleapis.com']
-    }
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+  },
 };
 
 export default nextConfig;
