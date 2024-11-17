984c349ab9299dfdb4f68088ed098c30cc49af17
Shivang Jain <129207095+shivangjain03@users.noreply.github.com>
2024-11-17 08:16:27 -0800

    Update route.ts

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -4,21 +4,21 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
-  const responce=await req.json();
+  const response = await req.json();
   try {
     const { data, error } = await resend.emails.send({
       from: 'easyshare@resend.dev',
       to: ['shivangjain2005@gmail.com'],
       subject: 'Hey! Your file is all set and waiting for you! 🗂️',
-      react: EmailTemplate({ responce }),
-    }); 
+      react: EmailTemplate({ response }),
+    });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return new Response(JSON.stringify({ error }), { status: 500 });
     }
 
-    return Response.json(data);
+    return new Response(JSON.stringify(data));
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return new Response(JSON.stringify({ error }), { status: 500 });
   }
 }
